966d73dfd9796d1c81cf6117e85479bc9e36b52f
z200801 <z200801@gmail.com>
2021-12-01 23:15:41 +0200

    ch1

--- 4_md5_recovery_files.sh
+++ 4_md5_recovery_files.sh
@@ -3,9 +3,11 @@
 #Search files in recovery directory and create md5 sum
 
 fname_md5="recovery_files.md5"
+CUR_DIR=`pwd`
 
 if [ "$1" ]; then
  fname_md5=$1
 fi
 
-find ./recovery -type f -name "*" -exec md5sum {} \; >$fname_md5
+cd recovery
+find . -type f -name "*" -exec md5sum {} \; >$CUR_DIR/$fname_md5
