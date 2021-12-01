c5f9dbb4517d0ff5e91a5882048399b8a3691437
z200801 <z200801@gmail.com>
2021-12-01 22:25:09 +0200

    ch1

--- 3_1_remove_all.sh
+++ 3_1_remove_all.sh
@@ -3,13 +3,7 @@
 BASE_DIR=~
 DIR_NAME="$BASE_DIR/mnt/test_fat32"
 
-fname_sizes="1M"
-
-MAX_DIR=2
-MAX_FILES=2
 TEST_DIRNAME="test"
-TEST_FILENAME="test_file"
-TEST_FILE_EXT="txt"
 
 #mkdir $DIR_NAME/$TEST_DIRNAME
 rm -rf $DIR_NAME/$TEST_DIRNAME
