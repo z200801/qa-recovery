67da81f38a93ca9341b728605a226079073cdcc5
z200801 <z200801@gmail.com>
2021-12-01 22:05:49 +0200

    ch1

--- 1_create_fat32.sh
+++ 1_create_fat32.sh
@@ -2,9 +2,12 @@
 
 SIZE=20
 FILENAME="test_fat32.img"
-DIR_NAME="/home/user/mnt/test_fat32"
+BASE_DIR=~
+DIR_NAME="$BASE_DIR/mnt/test_fat32"
 
+mkdir -p $DIR_NAME
 dd if=/dev/zero of=$FILENAME bs=1M count=$SIZE
+
 mkfs.vfat $FILENAME
 
 
