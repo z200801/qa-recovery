0ef54ae1abdaf65d28d638f8f06db076df13c8d2
z200801 <z200801@gmail.com>
2021-12-01 22:08:17 +0200

    ch1

--- 3_create_files.sh
+++ 3_create_files.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-DIR_NAME="/home/user/mnt/test_fat32"
+#Create test file with filling from /dev/urandom and calculate md5 sum
+
+BASE_DIR=~
+DIR_NAME="$BASE_DIR/mnt/test_fat32"
+MOUNT_FILENAME=`mount|grep \`users\`|grep mnt|awk {'print $1'}|sed 's/.*\///'`
 
 fname_sizes="1M"
 
@@ -8,9 +12,12 @@ MAX_DIR=2
 MAX_FILES=2
 TEST_DIRNAME="test"
 TEST_FILENAME="test_file"
-TEST_FILE_EXT="txt"
+TEST_FILE_EXT="dat"
+file_md5=$MOUNT_FILENAME.md5
 
 mkdir $DIR_NAME/$TEST_DIRNAME
+touch $file_md5
+
 
 for i_dir in `seq 1 $MAX_DIR`
 do
@@ -23,11 +30,15 @@ do
     fl2=$fl1"."$TEST_FILE_EXT
     fl3=$fl1"_new."$TEST_FILE_EXT
      echo $fl2
-     fallocate -l $fname_sizes $fl2
-     sed 's/[^~]/A/g' $fl2>$fl3
-     rm $fl2
+#     fallocate -l $fname_sizes $fl2
+#     sed 's/[^~]/A/g' $fl2>$fl3
+#     rm $fl2
+    dd if=/dev/urandom of=$fl2 bs=$fname_sizes count=1 status=none
+    md5sum $fl2 >>$file_md5
+
     done
 done
+
 
 
 
