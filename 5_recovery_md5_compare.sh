4069c2c0611c418a93eb46258c58b5239cbdb120
z200801 <z200801@gmail.com>
2021-12-01 23:15:59 +0200

    ch1

--- 5_recovery_md5_compare.sh
+++ 5_recovery_md5_compare.sh
@@ -2,24 +2,14 @@
 
 #Search files in recovery directory and create md5 sum
 
-fname_md5_recovery="recovery_files.md5"
-
 if [ -x "$1" ]; then
-    echo "Requre\n$0 [original_md5_file] {recovery_md5_file}"
+    echo "Requre\n$0 [original_md5_file] [recovery_md5_file]"
     exit 1
 fi
 
-fname_md5_original=$1
-
-if [ "$2" ]; then
- fname_md5_recovery=$2
+if [ -x "$2" ]; then
+    echo "Requre\n$0 [original_md5_file] [recovery_md5_file]"
+    exit 1
 fi
-
-ii=0
-fl_md5=`cat $fname_md5_recovery`
-for i in $fl_md5
-do
-    ii=$((${ii%}+1))
-    echo "ii=$ii"
-    echo $i
-done
+ echo "Compare recovery files"
+ cat $1 $2| sort |uniq -d
