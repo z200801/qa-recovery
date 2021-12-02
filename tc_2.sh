374f00f09e0b18297c3b5688ff8a14b04b0c3955
z200801 <z200801@gmail.com>
2021-12-02 10:43:40 +0200

    ch1

--- tc_2.sh
+++ tc_2.sh
@@ -9,23 +9,24 @@
 # 6. Create files with name deleted files
 # 7. Unmount container
 
+TESTCASE=2
 BASE_DIR=~
 CUR_DIR=`pwd`
 DIR_NAME="$BASE_DIR/mnt/test_fat32"
 MAX_DELETE_FILES=2
 FILENAME_ORIGINAL_CONTAINER="test_fat32.img"
-FILENAME_TC1="test_fat32_tc2.img"
+FILENAME_TC="test_fat32_tc$TESTCASE.img"
 fname_sizes="1M"
 
 # 1 copy original fs container
 echo "#1 Copy container"
-echo "$FILENAME_ORIGINAL_CONTAINER $FILENAME_TC1"
-cp $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC1
+echo "$FILENAME_ORIGINAL_CONTAINER $FILENAME_TC"
+cp $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC
 
 # 2 Mounting
-echo "#2 Mount new conteiner:$CUR_DIR/$FILENAME_TC1"
+echo "#2 Mount new conteiner:$CUR_DIR/$FILENAME_TC"
 mkdir -p $DIR_NAME
-sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC1 $DIR_NAME
+sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC $DIR_NAME
 
 # 3 Delete some files
 echo "#3 Delete some files"
@@ -47,9 +48,9 @@ sudo umount $DIR_NAME
 rm -rf $DIR_NAME
 
 # 5 Mounting existing conteiner
-echo "#5 Mount conteiner:$CUR_DIR/$FILENAME_TC1"
+echo "#5 Mount conteiner:$CUR_DIR/$FILENAME_TC"
 mkdir -p $DIR_NAME
-sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC1 $DIR_NAME
+sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC $DIR_NAME
 
 # 6 Create files with name deleted files
 echo "#6 Create files with name deleted files"
--- tc_3.sh
+++ tc_3.sh
@@ -9,23 +9,24 @@
 # 6. Create files with another name
 # 7. Unmount container
 
+TESTCASE=3
 BASE_DIR=~
 CUR_DIR=`pwd`
 DIR_NAME="$BASE_DIR/mnt/test_fat32"
 MAX_DELETE_FILES=2
 FILENAME_ORIGINAL_CONTAINER="test_fat32.img"
-FILENAME_TC1="test_fat32_tc2.img"
+FILENAME_TC="test_fat32_tc$TESTCASE.img"
 fname_sizes="1M"
 
 # 1 copy original fs container
 echo "#1 Copy container"
-echo "$FILENAME_ORIGINAL_CONTAINER $FILENAME_TC1"
-cp $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC1
+echo "$FILENAME_ORIGINAL_CONTAINER $FILENAME_TC"
+cp $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC
 
 # 2 Mounting
-echo "#2 Mount new conteiner:$CUR_DIR/$FILENAME_TC1"
+echo "#2 Mount new conteiner:$CUR_DIR/$FILENAME_TC"
 mkdir -p $DIR_NAME
-sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC1 $DIR_NAME
+sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC $DIR_NAME
 
 # 3 Delete some files
 echo "#3 Delete some files"
@@ -47,9 +48,9 @@ sudo umount $DIR_NAME
 rm -rf $DIR_NAME
 
 # 5 Mounting existing conteiner
-echo "#5 Mount conteiner:$CUR_DIR/$FILENAME_TC1"
+echo "#5 Mount conteiner:$CUR_DIR/$FILENAME_TC"
 mkdir -p $DIR_NAME
-sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC1 $DIR_NAME
+sudo mount --rw -t vfat -o uid=`users` $CUR_DIR/$FILENAME_TC $DIR_NAME
 
 # 6 Create files with another name
 echo "#6 Create files with another name"
@@ -60,10 +61,9 @@ do
   dd if=/dev/urandom of="$i.new" bs=$fname_sizes count=1 status=none
 done
 
-exit 0
-
 # 7 Unmount container
-echo "#5 Unmount container: $DIR_NAME"
+echo "#7 cd $BASE_DIR"
 cd $BASE_DIR
+echo "#7 Unmount container: $DIR_NAME"
 sudo umount $DIR_NAME
 rm -rf $DIR_NAME
