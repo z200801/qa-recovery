e03b99d3c48deb041440ab24b53d08482871b8c9
z200801 <z200801@gmail.com>
2021-12-03 22:14:01 +0200

    ch1

--- tc_6.sh
+++ tc_6.sh
@@ -7,8 +7,10 @@
 TESTCASE=6
 FILENAME_ORIGINAL_CONTAINER="test_fat32.img"
 FILENAME_TC="test_fat32_tc$TESTCASE.img"
-SEEK_TO_DAMAGE="0x100"
-COUNT_BYTE_TO_DAMAGE="128"
+#0x4 - 0x43 FAT0
+#0x44 - 0x83 FAT1
+SEEK_TO_DAMAGE="0x44"
+COUNT_BYTE_TO_DAMAGE="0x40"
 CHAR_TO_DAMAGE="\x01"
 
 # 1 copy original fs container
@@ -18,6 +20,7 @@ cp $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC
 
 # 6 Damage conteiner
 echo "#6 Damage conteiner"
-printf $CHAR_TO_DAMAGE | dd of=$FILENAME_TC bs=1 seek=$(($SEEK_TO_DAMAGE)) count=1 conv=notrunc
+#printf $CHAR_TO_DAMAGE | dd of=$FILENAME_TC bs=1 seek=$(($SEEK_TO_DAMAGE)) count=1 conv=notrunc
+dd if=/dev/urandom of=$FILENAME_TC bs=1 count=$(($COUNT_BYTE_TO_DAMAGE)) seek=$(($SEEK_TO_DAMAGE)) conv=notrunc status=none
 echo "#6 Compare files"
 cmp -b -c $FILENAME_ORIGINAL_CONTAINER $FILENAME_TC
