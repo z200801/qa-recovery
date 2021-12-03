2a3aef00b471a6eb267897699297d47f3abf2098
z200801 <z200801@gmail.com>
2021-12-04 01:46:35 +0200

    ch1

--- tc_1.sh
+++ tc_1.sh
@@ -125,7 +125,7 @@ create_files_new_names_in_container()
   cd $DIR_MOUNT ||   { echo "Failure #create_files_some_names_in_container "; exit 1; }
   for i in $file_will_be_delete
     do
-      echo "Create file:$i"
+      echo "Create file:$i.new"
       dd if=/dev/urandom of="$i.new" bs=$fname_sizes count=1 status=none
     done
   cd $CUR_DIR
