5f2f630a3cad74f8c3d4d0151d0e3f1c96b4c8e2
z200801 <z200801@gmail.com>
2021-12-11 19:42:23 +0200

    ch1

--- tc_function_unit.sh
+++ tc_function_unit.sh
@@ -18,10 +18,10 @@ SIZE_CONTAINER=20
 fname_sizes="1M"
 MAX_DIR=2
 MAX_FILES=2
-TEST_DIRNAME="test/win11"
+TEST_DIRNAME="test"
 TEST_FILENAME="test_file"
 TEST_FILE_EXT="db"
-RECOVERY_DIR="recovery"
+RECOVERY_DIR="win11/recovery"
 recovery_dir_array="diskdrill testdisk r-studio datarescue"
 
 # ==========================================================================================
